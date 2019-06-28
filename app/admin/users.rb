c5da79808aacf732726ca1c1e471fd9d236febb2
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2019-06-27 21:53:03 -0300

    add column birthdate to users

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  permit_params :registration, :name, :kind, :job_role, :status, :email, :password, :password_confirmation, :phone,
+  permit_params :registration, :name, :birthdate,:kind, :job_role, :status, :email, :password, :password_confirmation, :phone,
                 :addresses_attributes => [:id, :street, :number, :neighboard, :city, :state, :zip_code, :_destroy]
 
   index do
@@ -16,17 +16,22 @@ ActiveAdmin.register User do
     tag_column "Job Role" do |user|
       (user.job_role.nil?) ? user.job_role : user.job_role.upcase
     end
-    column :created_at
+    column "Birthdate" do |user|
+      user.birthdate.strftime("%d/%m/%Y")
+    end
     actions
   end
   show :title => proc {|user| user.to_s} do
     attributes_table do
       row :registration
       row :name
+      row "Birthdate" do |user|
+        user.birthdate.strftime("%d/%m/%Y")
+      end
       row :email
-      row :kind
-      row :job_role
-      row :status
+      tag_row :kind
+      tag_row :job_role
+      tag_row :status
       row :phone
       row "Address" do |user|
         user.addresses.each do |ad|
@@ -37,6 +42,7 @@ ActiveAdmin.register User do
   end
 
   filter :name
+  filter :birthdate
   filter :registration
   filter :email
   filter :kind, as: :select, collection: proc {User.kinds}
@@ -44,12 +50,13 @@ ActiveAdmin.register User do
   filter :status, as: :select, collection: proc {User.statuses}
   filter :email
   filter :addresses
-  filter :created_at
+
 
   form do |f|
     f.inputs "User Details" do
       f.input :registration
       f.input :name
+      f.input :birthdate, as: :datepicker
       f.input :kind
       f.input :job_role
       f.input :status
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_28_000905) do
+ActiveRecord::Schema.define(version: 2019_06_28_003957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2019_06_28_000905) do
     t.integer "kind"
     t.integer "job_role"
     t.string "phone"
+    t.date "birthdate"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
