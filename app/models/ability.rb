faa7ea0f862128cac22e095676c05b45e56ddcac
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2019-07-02 20:03:54 -0300

    config authorization with cancancan

---
+++ app/models/ability.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    if user.admin?
+      can :manage, :all
+    elsif user.teacher?
+      can [:update, :read], User, id: user.id
+      can :read, SchoolClass, user_id: user.id
+      can [:read], Student, school_classes: {user_id: user.id}
+      can :read, ActiveAdmin::Page, name: "Dashboard", namespace_name: "admin"
+    end
+  end
+end
