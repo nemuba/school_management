fde27231af13f205555894951e7e942e3f9a5455
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2019-07-04 12:52:21 -0300

    config db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name: "Admin", kind: :admin, job_role: :admin_user, birthdate: Time.now,email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+User.create!(name: "Admin", kind: :admin, job_role: :admin_user, birthdate: Time.now,
+             phone: "12997245865", adresses-attributes: {street: "street", number: "number",city: "city",
+                                                         state: "SP", zip_code: "11680-000", neighboard: "bairro"},
+             email: 'admin@example.com', password: 'password', password_confirmation: 'password')
