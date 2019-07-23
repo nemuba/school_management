4e10feedd10a4d114f85124cbb4f3fa47cca737c
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2019-07-23 01:09:46 -0300

    configurando dashboard e titulo do sistema

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,8 +4,10 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "School Management"
-
+  #config.site_title = I18n.t('active_admin.site_title')
+  config.namespace :admin do |admin|
+         admin.site_title = "Sistema Escolar"
+  end
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
